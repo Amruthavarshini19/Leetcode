f754e7f7694a892e1d8905a194a8c93acf5706e0
Amruthavarshini19 <amruthavpt@gmail.com>
2026-09-02 10:11:10 +0530

    Time: 192 ms (77.33%), Space: 16.3 MB (48%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def scheduleCourse(self, courses):
+        courses.sort(key=lambda x:x[1])
+        heap = []
+        time = 0
+        for duration, deadline in courses:
+            time+=duration
+            heapq.heappush(heap, -duration)
+            if time>deadline:
+                longest = -heapq.heappop(heap)
+                time-=longest
+        return len(heap)
+        
